1560b2a2303dab187e239f6108334cd206e1d46a
toxzwaza <84907740+toxzwaza@users.noreply.github.com>
2025-01-13 11:53:34 +0900

    エラーメッセージ対応

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\Computer;
 use App\Models\Data;
+use Exception;
 use Illuminate\Http\Request;
 
 class DataController extends Controller
@@ -13,33 +14,38 @@ class DataController extends Controller
     public function store(Request $request)
     {
         $status = false;
+        $message = '';
 
         $temperature = $request->temperature;
         $humidity = $request->humidity;
         $co2 = $request->co2;
         $place_id = $request->place_id;
 
-        if($temperature && $humidity && $co2 && $place_id){
-            $data = new Data();
-            $data->temperature = $temperature;
-            $data->humidity = $humidity;
-            $data->co2 = $co2;
-            $data->place_id = $place_id;
-            $data->save();
-            $status = true;
+        try {
+            if ($temperature && $humidity && $co2 && $place_id) {
+                $data = new Data();
+                $data->temperature = $temperature;
+                $data->humidity = $humidity;
+                $data->co2 = $co2;
+                $data->place_id = $place_id;
+                $data->save();
+                $status = true;
+            }
+        } catch (Exception $e) {
+            $msg = $e->getMessage();
         }
 
-        return response()->json(['status' => $status]);
-
+        return response()->json(['status' => $status, 'msg' => $msg ]);
     }
 
-    public function getPlaceId(Request $request){
+    public function getPlaceId(Request $request)
+    {
         $mac_address = $request->mac_address ?? '';
 
         $place = Computer::where('mac_address', $mac_address)->first();
-        if($place){
+        if ($place) {
             return response()->json(['placeId' => $place->id]);
-        }else{
+        } else {
             return response()->json(['placeId' => 0]);
         }
     }
