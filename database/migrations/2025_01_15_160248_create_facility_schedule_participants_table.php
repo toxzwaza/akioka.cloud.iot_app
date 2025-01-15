47f399f1b37a91b0d48ab976dcc1eab6d983907b
toxzwaza <84907740+toxzwaza@users.noreply.github.com>
2025-01-15 16:48:59 +0900

    参加者登録用DBマイグレーション

---
+++ database/migrations/2025_01_15_160248_create_facility_schedule_participants_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('facility_schedule_participants', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('facility_schedule_id')->constrained('facility_schedules');
+            $table->string('user_name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('facility_schedule_participants');
+    }
+};
