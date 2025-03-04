694f83c38add77b670df8bb4390ca420d00a0d0c
toxzwaza <84907740+toxzwaza@users.noreply.github.com>
2025-03-04 14:47:46 +0900

    食堂サイネージ フォントサイズ変更

--- resources/js/Pages/Content/ReceiveCompleteBack.vue
+++ resources/js/Pages/Content/ReceiveCompleteBack.vue
@@ -30,10 +30,10 @@ if (props.initial_orders.length > ordersPerPage) {
 </script>
 <template>
   <div id="signage_content" class="w-full bg-gray-50">
-    <h1 class="text-center py-8  text-2xl font-bold text-white">
+    <h1 class="text-center py-4  text-2xl font-bold text-white">
       <span class=" text-red-500 inline-block mr-4">{{ props.place_name }}</span>の納品完了済みデータを表示中
     </h1>
-    <p class="mb-8 text-center text-lg text-white font-bold underline">
+    <p class="mb-4 text-center text-lg text-white font-bold underline">
       自分の納品物が表示されている場合、該当の納品場所まで取りに来てください。
     </p>
 
@@ -58,13 +58,13 @@ if (props.initial_orders.length > ordersPerPage) {
             :key="order.id"
             class="text-center bg-black"
           >
-            <td class="w-1/5 font-bold py-8 px-4 border-b border-gray-200 text-left">
+            <td class="w-1/5 font-bold py-4 px-4 border-b border-gray-200 text-left">
               {{ order.order_user }}
             </td>
-            <td class="w-3/5  font-bold py-8 px-4 border-b border-gray-200 text-left">
+            <td class="w-3/5  font-bold py-4 px-4 border-b border-gray-200 text-left">
               {{ order.name.length > 15 ? order.name.slice(0, 15) + '...' : order.name }}
             </td>
-            <td class="w-1/5  font-bold py-8 px-4 border-b border-gray-200 text-left">
+            <td class="w-1/5  font-bold py-4 px-4 border-b border-gray-200 text-left">
               {{ order.s_name.length > 15 ? order.s_name.slice(0, 15) + '...' : order.s_name }}
             </td>
             <!-- <td class="text-4xl font-bold py-8 px-4 border-b border-gray-200">
