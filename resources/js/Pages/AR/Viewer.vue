0e264586d56effcab7964f36204a654da26d995a
toxzwaza <84907740+toxzwaza@users.noreply.github.com>
2025-05-21 13:39:22 +0900

    AR:サンプルver1.0

--- resources/js/Pages/AR/Viewer.vue
+++ resources/js/Pages/AR/Viewer.vue
@@ -1,4 +1,26 @@
-<!-- resources/js/Pages/ArView.vue -->
+<script setup>
+import { onMounted } from "vue";
+
+onMounted(() => {
+  setTimeout(() => {
+    const videoElement = document.querySelector("video");
+    if (videoElement) {
+      const videoHeight = videoElement.getBoundingClientRect().height;
+      const screenHeight = window.innerHeight;
+      const descriptionHeight = screenHeight - videoHeight;
+      document.getElementById(
+        "description"
+      ).style.height = `${descriptionHeight}px`;
+      console.log("Actual video height:", videoHeight);
+      console.log("all inner height:", window.innerHeight);
+      console.log("Description height:", descriptionHeight);
+    } else {
+      console.log("Video element not found.");
+    }
+  }, 3000);
+});
+</script>
+
 <template>
   <div v-once>
     <a-scene
@@ -13,7 +35,7 @@
         <a-asset-item id="3dmodel" src="/model.glb"></a-asset-item>
       </a-assets>
 
-      <a-camera position="0 0 0" look-controls="enabled: false"></a-camera>
+      <a-camera position="0 0 0" look-controls="enabled: false"> </a-camera>
 
       <a-entity mindar-image-target="targetIndex: 0">
         <a-gltf-model
@@ -25,41 +47,70 @@
       </a-entity>
     </a-scene>
   </div>
+  <span id="left_up_ar_icon">AR</span>
+
+  <img id="bn_1" src="/bn_1.png" alt="" />
 
   <div id="description">
-    <p class="text-white font-bold p-2">製品の説明や会社HPへのリンクを表示。</p>
-
+    <p class="text-gray-400 p-2">製品の説明や会社HPへのリンクを表示。</p>
+    <p class="text-gray-400 p-2">アクセス数をカウントする。</p>
   </div>
 </template>
 
-<script setup>
-// コンポーネントのスクリプトは不要です（A-Frameが自動実行するため）
-</script>
-<style lang="scss">
+<style lang="scss" >
+body {
+  background-image: url("ar_bg.png");
+}
 .mindar-ui-loading {
   display: none !important;
   & .loader {
     display: none !important;
   }
-
-  
+}
+.a-enter-vr-button {
+  display: none !important;
 }
 
-video{
- height: 80vh !important;  
- object-fit: cover;
+video {
+  height: 80vh !important;
+  object-fit: cover;
 }
 
-#description{
-    position: fixed;
-    bottom: 0;
-    left: 0;
-    z-index: 100;
-
-    width: 100vw;
-    height: 20vh;
-    background-color: rgb(69, 69, 255);
+#left_up_ar_icon {
+  position: fixed;
+  top: 2%;
+  left: 2%;
+  display: block;
+  background-color: red;
+  color: white;
+  padding: 0.6rem;
+  font-weight: bold;
+  border-radius: 3px;
+  opacity: 0.8;
+  font-size: 0.7rem;
 }
 
+#description {
+  position: fixed;
+  bottom: 2%;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 100;
+  padding: 1rem;
 
+  width: 90vw;
+  margin: 0 auto;
+  //   height: 20vh;
+  background-color: white;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  border-radius: 3px;
+}
+
+#bn_1 {
+  width: 30vw;
+  position: fixed;
+  right: 2%;
+  top: 2%;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+}
 </style>
