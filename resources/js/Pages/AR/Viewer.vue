f14e285469f2b61d1179502f8bb88988207f655e
toxzwaza <84907740+toxzwaza@users.noreply.github.com>
2025-05-21 11:24:18 +0900

    AR:カメラ調整

--- resources/js/Pages/AR/Viewer.vue
+++ resources/js/Pages/AR/Viewer.vue
@@ -25,8 +25,41 @@
       </a-entity>
     </a-scene>
   </div>
+
+  <div id="description">
+    <p class="text-white font-bold p-2">製品の説明や会社HPへのリンクを表示。</p>
+
+  </div>
 </template>
 
 <script setup>
 // コンポーネントのスクリプトは不要です（A-Frameが自動実行するため）
 </script>
+<style lang="scss">
+.mindar-ui-loading {
+  display: none !important;
+  & .loader {
+    display: none !important;
+  }
+
+  
+}
+
+video{
+ height: 80vh !important;  
+ object-fit: cover;
+}
+
+#description{
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    z-index: 100;
+
+    width: 100vw;
+    height: 20vh;
+    background-color: rgb(69, 69, 255);
+}
+
+
+</style>
