8e51ab1dc106346e9396f690435a101486247be8
toxzwaza <84907740+toxzwaza@users.noreply.github.com>
2025-05-21 10:44:09 +0900

    vite.configにarモジュール排除追記

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: "resources/js/app.js",
             refresh: true,
         }),
         vue({
@@ -17,11 +17,19 @@ export default defineConfig({
             },
         }),
     ],
+    optimizeDeps: {
+        exclude: ["aframe", "mind-ar"], // ←依存の最適化から除外
+    },
     css: {
         preprocessorOptions: {
-          scss: {
-            silenceDeprecations: ["legacy-js-api"],
-          },
+            scss: {
+                silenceDeprecations: ["legacy-js-api"],
+            },
         },
-      },
+    },
+    build: {
+        rollupOptions: {
+            external: ["aframe", "mind-ar"], // ←バンドル対象から除外
+        },
+    },
 });
