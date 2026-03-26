868e94182dac2d0e45c7c115b703623c646af8fc
村上 飛羽 <to-murakami@akioka-ltd.jp>
2026-03-26 09:46:39 +0900

    testルートを棚卸データ出力用に変更

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -81,49 +81,49 @@ class TestController extends Controller
 
         // return $res;
 
-        // $stocks = Stock::select(
-        //     'stocks.id', 
-        //     'suppliers.name as supplier_name', 
-        //     'stocks.name', 
-        //     'stocks.s_name', 
-        //     'stocks.memo', 
-        //     'stocks.price', 
-        //     'stocks.solo_unit', 
-        //     'stocks.org_unit', 
-        //     'stocks.quantity_per_org',
-        //     'stocks.updated_at'
-        // )
-        // ->join('stock_suppliers', 'stock_suppliers.stock_id', '=', 'stocks.id')
-        // ->join('suppliers', 'suppliers.id', '=', 'stock_suppliers.supplier_id')
-        // ->where('stocks.del_flg', '=', 0)
-        // ->whereNotNull('stocks.price')
-        // ->get();
+        $stocks = Stock::select(
+            'stocks.id', 
+            'suppliers.name as supplier_name', 
+            'stocks.name', 
+            'stocks.s_name', 
+            'stocks.memo', 
+            'stocks.price', 
+            'stocks.solo_unit', 
+            'stocks.org_unit', 
+            'stocks.quantity_per_org',
+            'stocks.updated_at'
+        )
+        ->join('stock_suppliers', 'stock_suppliers.stock_id', '=', 'stocks.id')
+        ->join('suppliers', 'suppliers.id', '=', 'stock_suppliers.supplier_id')
+        ->where('stocks.del_flg', '=', 0)
+        ->whereNotNull('stocks.price')
+        ->get();
 
-        // $csvData = $stocks->map(function($stock) {
-        //     return [
-        //         $stock->supplier_name,
-        //         $stock->name,
-        //         $stock->s_name,
-        //         $stock->memo,
-        //         $stock->price,
-        //         $stock->solo_unit,
-        //         $stock->org_unit,
-        //         $stock->quantity_per_org,
-        //         $stock->updated_at
-        //     ];
-        // });
+        $csvData = $stocks->map(function($stock) {
+            return [
+                $stock->supplier_name,
+                $stock->name,
+                $stock->s_name,
+                $stock->memo,
+                $stock->price,
+                $stock->solo_unit,
+                $stock->org_unit,
+                $stock->quantity_per_org,
+                $stock->updated_at
+            ];
+        });
 
-        // $fileName = 'stocks.csv';
-        // $file = fopen($fileName, 'w');
-        // fputcsv($file, ['発注先名', '品名', '品番', 'メモ', '価格', '単位１', '単位２', '換算値', '更新日時']);
+        $fileName = 'stocks.csv';
+        $file = fopen($fileName, 'w');
+        fputcsv($file, ['発注先名', '品名', '品番', 'メモ', '価格', '単位１', '単位２', '換算値', '更新日時']);
 
-        // foreach ($csvData as $line) {
-        //     fputcsv($file, $line);
-        // }
+        foreach ($csvData as $line) {
+            fputcsv($file, $line);
+        }
 
-        // fclose($file);
+        fclose($file);
 
-        // return response()->download($fileName)->deleteFileAfterSend(true);
+        return response()->download($fileName)->deleteFileAfterSend(true);
 
     }
 
